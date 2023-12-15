9b3b9a4f97c56cc16833e9cc8b7acb7f765e4aef
Jack Hermanson <jack.hermanson@live.com>
2023-12-14 21:02:32 -0700

    search form wip

--- main/modules/chore_logs/forms.py
+++ main/modules/chore_logs/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import SubmitField, DateField
+from wtforms.fields import SubmitField, DateField, StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class ChoreLogDueDate(FlaskForm):
     due_date = DateField(format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField("Save")
+
+
+class SearchAndFilterForm(FlaskForm):
+    search_text = StringField(label="Search")
+    show_archived = BooleanField(label="Show Archived")
+    submit = SubmitField("Submit")
